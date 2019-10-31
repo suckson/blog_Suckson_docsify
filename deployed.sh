85f2ad0d596f87c8ee28399128675fb790c910d6
suckson <chaixiaoheng@aliyun.com>
2019-10-31 15:14:33 +0800

    add pm2.js

--- deployed.sh
+++ deployed.sh
@@ -1,3 +1,11 @@
-cd /front/docs/
+### 
+# @Descripttion: 
+ # @version: 
+ # @Author: suckson
+ # @Date: 2019-10-31 14:53:30
+ # @LastEditors: suckson
+ # @LastEditTime: 2019-10-31 15:02:26
+ ###
+cd /usr/local/project/node-blog-server/views/
 
 git pull origin master
